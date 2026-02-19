c471e2446456bc4f33c453ec975b2a642317a55a
josegcpa <jose.almeida@research.fchampalimaud.org>
2026-02-19 13:32:55 +0000

    ignoring depecration warnings from other packages

--- src/nnunet_serve/__init__.py
+++ src/nnunet_serve/__init__.py
@@ -1,4 +1,16 @@
 import os
+import warnings
+
+IGNORE_WARNINGS = [
+    "Please import `label` from the `scipy.ndimage` namespace;",
+    "Please import `grey_dilation` from the `scipy.ndimage` namespace;",
+    "Please import `gaussian_gradient_magnitude` from the `scipy.ndimage` namespace;",
+    "Please import `gaussian_filter` from the `scipy.ndimage` namespace;",
+    "DeprecationWarning: builtin type SwigPyPacked has no __module__ attribute",
+]
+for warn in IGNORE_WARNINGS:
+    warnings.filterwarnings("ignore", message=warn)
+
 
 for k in ["nnUNet_preprocessed", "nnUNet_raw", "nnUNet_results"]:
     dir = "tmp" if k != "nnUNet_preprocessed" else "tmp/preproc"
